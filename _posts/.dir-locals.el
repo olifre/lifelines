1c4a1962750aa685fc675b06c3656d017754e055
Oliver Freyermuth <o.freyermuth@googlemail.com>
2021-02-14 21:48:17 +0100

    Add .dir-locals for emacs.

---
+++ _posts/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((markdown-mode . ((eval . (flyspell-mode 1))
+									 (ispell-local-dictionary . "british-ize"))))
